376c6ec726186eee710d578b4eef104cda8b9849
W1ckh3ad <69201877+W1ckh3ad@users.noreply.github.com>
2023-01-16 00:40:12 +0100

    update index ts imports where missing

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export {
+  default as SearchStateContext,
+  useGetSearchState,
+  useSetSearchState,
+} from "./context";
+export * from "./hooks";
+export * from "./types";
+export * from "./utils";
