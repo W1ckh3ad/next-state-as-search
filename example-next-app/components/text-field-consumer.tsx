f2186eb6dcdfb2237bdf23c6760294fbadcaaa2b
W1ckh3ad <69201877+W1ckh3ad@users.noreply.github.com>
2023-01-16 21:50:05 +0100

    setting up example project

---
+++ example-next-app/components/text-field-consumer.tsx
@@ -0,0 +1,8 @@
+export default function TextFieldConsumer() {
+  return (
+    <div>
+      <label>Test</label>
+      <input value={""} onChange={() => {}} />
+    </div>
+  );
+}
---
+++ example-next-app/pages/withHocContext.tsx
@@ -0,0 +1,10 @@
+function WithHocContext() {
+  return (
+    <div>
+      <h1>With Context Page</h1>
+      <section></section>
+    </div>
+  );
+}
+
+export default () => <WithHocContext />;
